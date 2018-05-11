ed24952a08ef9086826d3b4c3f27a0597f65695f
noeliavallez <noelia.vallez@uclm.es>
2018-05-11 12:24:17 +0200

    integrated files

--- tools/objectdetection_train_CaffeBonseyes/actions.py
+++ tools/objectdetection_train_CaffeBonseyes/actions.py
@@ -16,106 +16,66 @@ from com_bonseyes_base.formats.data.blob.api import BlobDataEditor
 from com_bonseyes_base.formats.data.data_tensors.api import DataTensorsViewer, DimensionNames
 from com_bonseyes_base.lib.api.tool import Context
 from com_bonseyes_base.lib.impl.utils import execute_with_logs
-
-def download(url, output_file):
-	log.info (url)
-	if os.path.exists(output_file):
-		log.info ('Output file already exists')
-	else:
-
-		ret = requests.get(url, stream=True)
-		if ret.status_code != 200:
-			raise Exception('Unable to file (error code %d)' % ret.status_code)
-
-		with open(output_file, 'wb') as fp:
-			for chunk in ret.iter_content(chunk_size=1024 * 1024):
-				if chunk:
-					fp.write(chunk)
+from bonseyes_youtubebb.mobilenetSSD import proto_generator_BonseyesCaffe, solver_generator_BonseyesCaffe
 
 
-def perform_training_caffe(context: Context[BlobDataEditor], model: BlobDataEditor, training_set: DataTensorsViewer, epochs: str, batch: str):
+def perform_training_caffe(context: Context[BlobDataEditor], model: BlobDataEditor, training_set: DataTensorsViewer, epochs: str, batch_size: str):
 
-	#with context.data.edit_content() as output_path:
-	with tempfile.TemporaryDirectory() as tmp_dir:
-		#os.mkdir (output_path)
-		with zipfile.ZipFile(training_set, 'r') as z:
-			z.extractall(tmp_dir)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        with  model.view_content() as model_zip:
+            with zipfile.ZipFile(model_zip, 'r') as z:
+                z.extractall(tmp_dir)
 
-		with  model.view_content() as model_zip:
-			with zipfile.ZipFile(model_zip, 'r') as z:
-				z.extractall(tmp_dir)
+        log.info(training_set)
+        with zipfile.ZipFile(training_set, 'r') as z:
+            z.extractall(tmp_dir)
 
-		download('http://161.67.219.121/BONSEYES_Reference_Datasets/YoutubeBB/training/labelmap_youtubebb.prototxt', os.path.join ("/volumes/data", "labelmap_youtubebb.prototxt"))
-		download("http://161.67.219.121/BONSEYES_Reference_Datasets/YoutubeBB/training/gen_CaffeBonseyes.py", os.path.join("/volumes/data", "gen_CaffeBonseyes.py"))
-		download("http://161.67.219.121/BONSEYES_Reference_Datasets/YoutubeBB/training/solverGenerator_CaffeBonseyes.py", os.path.join("/volumes/data", "solverGenerator_CaffeBonseyes.py"))
+        shutil.copy2(os.path.join("/volumes/data", "labelmap_youtubebb.prototxt"),
+                     os.path.join(tmp_dir, "labelmap_youtubebb.prototxt"))
+        labelmap_path = os.path.join(tmp_dir, "labelmap_youtubebb.prototxt")
+        lmdb_path = os.path.join(tmp_dir, "youtube-bb_trainval_lmdb")
 
-		shutil.copy2(os.path.join("/volumes/data", "labelmap_youtubebb.prototxt"),os.path.join(tmp_dir, "labelmap_youtubebb.prototxt"));
-		shutil.copy2(os.path.join("/volumes/data", "gen_CaffeBonseyes.py"),os.path.join(tmp_dir, "gen_CaffeBonseyes.py"));
-		shutil.copy2(os.path.join("/volumes/data", "solverGenerator_CaffeBonseyes.py"),os.path.join(tmp_dir, "solverGenerator_CaffeBonseyes.py"));
-		
-		labelmap_path = os.path.join(tmp_dir, "labelmap_youtubebb.prototxt")
-		lmdb_path = os.path.join(tmp_dir, "youtube-bb_trainval_lmdb")
+        # ================Train.prototxt================
+        train_path = os.path.join(tmp_dir, "MobileNetSSD_train.prototxt")
+        proto_generator_BonseyesCaffe(train_path, "train", lmdb_path, labelmap_path, 24, int(batch_size), False, 22)
 
-		#================Train.prototxt================
-		cmd = ['python', os.path.join(tmp_dir, "gen_CaffeBonseyes.py"), "-s", "train", "-d", lmdb_path, "-l", labelmap_path, "-c", "24", "-b", batch]
+        # ================Solver.prototxt================
 
-		process = subprocess.Popen(cmd, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		process.wait()
-		train_path = os.path.join(tmp_dir, "MobileNetSSD_train.prototxt")
-		proto = open(train_path, 'wb')
-		output, errors = process.communicate()
-		log.info(errors)
+        solver_path = os.path.join(tmp_dir, "solver.prototxt")
+        solver_generator_BonseyesCaffe(solver_path, train_path, int(epochs), tmp_dir + "/", "detection")
 
-		proto.write(output)
+        log.info("Start train")
 
-		proto.flush()
-		proto.close()
-		#================Train.prototxt================
+        real_weights_config_path = os.path.join(tmp_dir, "trained-model.caffemodel")
 
-        #================Solver.prototxt================
-		cmd = ['python', os.path.join(tmp_dir, "solverGenerator_CaffeBonseyes.py"), "-n", train_path, "-it", epochs, "-o", tmp_dir + "/"]
-		process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-		process.wait()
+        full_command = ['/opt/caffe/build/tools/caffe', 'train', '--solver=' + solver_path,
+                        '--weights=' + real_weights_config_path, '--gpu', '0']
+        # # /media/mila/DATOS/BONSEYES/object_detection/MobileNet-SSD/caffe-ssd/build/tools/caffe train -solver solver.prototxt -weights MobileNetSSD_deploy.caffemodel -gpu 0
+        # # '--weights='+tmp_dir+'MobileNetSSD_deploy.caffemodel',
+        process = subprocess.Popen(full_command, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                   close_fds=True)  # , shell=True)
+        for line in process.stdout:
+            log.info(line.decode('utf-8', 'ignore').strip())
 
-		solver_path = os.path.join(tmp_dir, "solver2.prototxt")
-		proto = open(solver_path, 'wb')
-		stdout, stderr = process.communicate()
-		proto.write(stdout)
-
-		proto.flush()
-		proto.close()
-
-        #================Solver.prototxt================
-
-		log.info("Start train")
-
-		weights_path = os.path.join(tmp_dir, "trained-model.caffemodel")
-		full_command = ['/opt/caffe/build/tools/caffe', 'train', '--solver=' + solver_path, '--weights='+ weights_path, '--gpu', '0']
-
-		process = subprocess.Popen(full_command, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)#, shell=True)
-
-		for line in process.stdout:
-			log.info(line.decode('utf-8', 'ignore').strip())
-
-		process.wait()
-		output, errors = process.communicate()
-		log.info(".....................................")
-		log.info(errors)
-		with context.data.edit_content() as output_file:
-			with zipfile.ZipFile(output_file, 'a') as z:
-				z.write(train_path, arcname='MobileNetSSD_train.prototxt')
-				z.write(solver_path, arcname='solver.prototxt')
-				z.write(os.path.join(tmp_dir, 'snapshot_iter_' + epochs + '.caffemodel'), arcname='trained-model.caffemodel')
-				z.write(os.path.join(tmp_dir, 'snapshot_iter_' + epochs + '.solverstate'), arcname='trained-model.solverstate')
+        process.wait()
+        output, errors = process.communicate()
+        log.info(".....................................")
+        log.info(errors)
+        with context.data.edit_content() as output_file:
+            with zipfile.ZipFile(output_file, 'a') as z:
+                z.write(train_path, arcname='MobileNetSSD_train.prototxt')
+                z.write(solver_path, arcname='solver.prototxt')
+                z.write(os.path.join(tmp_dir, 'snapshot_iter_' + epochs + '.caffemodel'),
+                        arcname='trained-model.caffemodel')
+                z.write(os.path.join(tmp_dir, 'snapshot_iter_' + epochs + '.solverstate'),
+                        arcname='trained-model.solverstate')
 
 
+def create(context: Context[BlobDataEditor], model: BlobDataEditor, training_set: DataTensorsViewer,
+           epochs: str = "28000", batch_size: str = "12"):  # BlobDataEditor
 
-def create(context: Context[BlobDataEditor], model: BlobDataEditor, training_set: DataTensorsViewer, epochs: str="28000", batch="24"):#BlobDataEditor
+    log.info(training_set)
+    lmdb_path = '/' + training_set.split('/')[1] + '/' + training_set.split('/')[2] + '/data'
+    log.info(lmdb_path)
 
-	print ('hola ')
-	print (epochs)
-	log.info (training_set)
-	lmdb_path =  '/'+training_set.split('/')[1]+'/'+training_set.split('/')[2]+'/data'
-	log.info(lmdb_path)
-
-	perform_training_caffe (context, model, training_set, epochs, batch)
+    perform_training_caffe(context, model, training_set, epochs, batch_size)
